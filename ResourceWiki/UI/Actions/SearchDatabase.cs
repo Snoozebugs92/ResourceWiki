449a57c6f77fe427c2567207cb578476b6abf748
Snoozebugs92 <100491452+Snoozebugs92@users.noreply.github.com>
2022-06-07 17:33:27 +0200

    Add project files.

---
+++ ResourceWiki/UI/Actions/SearchDatabase.cs
@@ -0,0 +1,25 @@
+﻿using Dapper;
+using ResourceWiki.Database;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResourceWiki.UI.Actions
+{
+    public  class SearchDatabase
+    {
+        internal List<Subject> SearchStuff(string searchInput)
+        {
+            var connectionString = "xxxxx";
+            var sql = $"select * from dbo.Wiki where Chapter like '%{searchInput}%'";
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                return connection.Query<Subject>(sql).ToList();
+            }
+        }
+    }
+}
---
+++ ResourceWiki/UI/Actions/ShowResults.cs
@@ -0,0 +1,25 @@
+﻿using Dapper;
+using ResourceWiki.Database;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResourceWiki.UI.Actions
+{
+    public class ShowResults
+    {
+        internal List<TextMatter> DisplayStuff(string selectInput)
+        {
+            var connectionString = "xxxxxx";
+            var sql = $"select Text from dbo.Wiki where Chapter like '%{selectInput}%'";
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                return connection.Query<TextMatter>(sql).ToList();
+            }
+        }
+    }
+}
